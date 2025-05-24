d32aa039f09a3ca204ed9c952e75c7066b281b69
mansimittal09 <mansimittal983@gmail.com>
2025-05-24 19:52:25 +0530

    Add files via upload

---
+++ new.js
@@ -0,0 +1,64 @@
+// 1. Check if a number is positive, negative, or zero:  
+// Write a Java Script program that checks if a given number is positive, negative, or zero
+
+
+let a=5;
+ 
+if(a>0){
+    console.log("it is positive");
+}
+else{
+    console.log("it is negative");
+}
+if(a==0){
+    console.log("it is zero");
+}
+
+//2. Even or Odd:  
+//Create a Java Script program that determines if a number is even or odd using `if-else`.
+
+let i =5;
+
+if(i%2==0){
+    console.log("it is even")
+}
+else{
+    console.log("it is odd")
+}
+
+// Write a Java Script program to check if a user is eligible to vote based on their age (age >= 18)
+
+
+let b = 18
+
+if(b>=18){
+    console.log("it can vote")
+}
+else{
+    console.log("it cannot vote")
+}
+
+// Write a program to find the larger of two numbers using `if-else`
+
+let c = 3
+ let d = 22
+if( c>=d){
+    console.log(" c is larger")
+}
+else{
+    console.log(" c is smaller")
+}
+    
+//Create a Java Script program to check if a given number is divisible by 5.
+
+let div = 23
+
+if( div%5==0){
+    console.log(" it is divisible by 5")
+}
+else{
+    console.log(" it is not divisible by 5")
+} 
+
+// Write a Java Script program that checks if a given character (a, e, i, o, u) is a vowel or consonant.
+    
